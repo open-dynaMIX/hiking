84a1e7cad44738d5001ece8122cd36aad8918f0a
Fabio Ambauen <1833932+open-dynaMIX@users.noreply.github.com>
2022-10-19 20:39:04 +0200

    chore: cleanup

--- hiking/db_utils.py
+++ hiking/db_utils.py
@@ -9,7 +9,7 @@ Base = declarative_base()
 engine = create_engine(
     f"sqlite:///{str(DB_PATH.absolute())}"
     if not os.environ.get("HIKING_TEST")
-    else "sqlite://"  # , echo=True
+    else "sqlite://"
 )
 Session = sessionmaker(bind=engine)
 session = Session()
